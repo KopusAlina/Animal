6beb22d0f33253bcab74c34cc9bfd95532736683
Alina Kopus <botton_of_the_ocean@mail.ru>
2024-02-18 23:12:57 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Animal
+# Animal
---
+++ src/Cat.java
@@ -0,0 +1,7 @@
+public class Cat extends Animal {
+    public void sound(){
+        System.out.println("Мяу");
+        Cat cat = new Cat();
+        cat.sound();
+    }
+}
